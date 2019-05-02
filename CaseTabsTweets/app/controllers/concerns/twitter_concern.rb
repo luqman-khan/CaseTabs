d8f2d8111f8666cb8200f3e91695bebce8fce57a
bitcraze <bitcraze@bitcraze-vm.unassigned-domain>
2019-05-02 03:34:18 +0200

    adding link

--- CaseTabsTweets/app/controllers/concerns/twitter_concern.rb
+++ CaseTabsTweets/app/controllers/concerns/twitter_concern.rb
@@ -9,4 +9,16 @@ module TwitterConcern
 		end
 		return client
 	end
+	def convert_to_tweets tweets_obj
+		tweets = []
+		tweets_obj.each do |tweet|
+			page_names = Set.new tweet.text.scan(/@[^\s]+/)
+			text = tweet.text
+			page_names.each do |page_name|
+				text = tweet.text.gsub(page_name,"<a href=#{home_path(page_name)} class=card-link>#{page_name}</a>")
+			end
+			tweets.push({date: tweet.created_at.to_date, text: text})
+		end
+		return tweets
+	end
 end
--- CaseTabsTweets/app/controllers/home_controller.rb
+++ CaseTabsTweets/app/controllers/home_controller.rb
@@ -1,11 +1,26 @@
 class HomeController < ApplicationController
 	include TwitterConcern
+	before_action :set_page_name, only: [:show]
+
 	def index
-		# @tweets = [{title: "Luqman", text: "Computer Scientist"},{title: "Sulaiman", text: "Computer Engineer"},{title: "Sulaiman", text: "Computer Engineer"},{title: "Sulaiman", text: "Computer Engineer"}]
-		
-		@tweets = Rails.cache.fetch("recent_tweets", expires_in: 5.minutes) do
-			twitter_client.user_timeline("Casetabs", count:20, exclude_replies: true, include_rts: false, result_type: "recent")
+		@tweets_obj = Rails.cache.fetch("Casetabs_recent_tweets", expires_in: 5.minutes) do
+			twitter_client.user_timeline("@Casetabs", count:20, exclude_replies: true, include_rts: false, result_type: "recent")
 		end
-		aaa
+		# @tweets_obj=twitter_client.user_timeline("@LuqmanK55185700", count:20, exclude_replies: true, include_rts: false, result_type: "recent")
+		@tweets= convert_to_tweets @tweets_obj
+	end
+
+	def show
+		@tweets_obj = Rails.cache.fetch("#{@page_name}_recent_tweets", expires_in: 5.minutes) do
+			twitter_client.user_timeline(@page_name, count:20, exclude_replies: true, include_rts: false, result_type: "recent")
+		end
+		@tweets= convert_to_tweets @tweets_obj
+		render :index
+	end
+
+	private
+
+	def set_page_name
+		@page_name = params[:id]
 	end
 end
