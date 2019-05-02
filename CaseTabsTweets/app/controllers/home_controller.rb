ae3076da6321ae1e7101d2e0553b2b0806fdd046
bitcraze <bitcraze@bitcraze-vm.unassigned-domain>
2019-05-02 08:17:57 +0200

    adding tweet name to view

--- CaseTabsTweets/app/controllers/home_controller.rb
+++ CaseTabsTweets/app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
 class HomeController < ApplicationController
 	include TwitterConcern
-	before_action :set_page_name, only: [:show]
+	before_action :set_page_name
 
 	def index
-		@tweets_obj = Rails.cache.fetch("Casetabs_recent_tweets", expires_in: 5.minutes) do
-			twitter_client.user_timeline("@Casetabs", count:20, exclude_replies: true, include_rts: false, result_type: "recent")
+		@tweets_obj = Rails.cache.fetch("#{@page_name}_recent_tweets", expires_in: 5.seconds) do
+			twitter_client.user_timeline(@page_name, count:20, exclude_replies: true, include_rts: false, result_type: "recent")
 		end
+
+		## Use the line below if tweets does not contain user name
 		# @tweets_obj=twitter_client.user_timeline("@LuqmanK55185700", count:20, exclude_replies: true, include_rts: false, result_type: "recent")
+		
 		@tweets= convert_to_tweets @tweets_obj
 	end
 
@@ -21,6 +24,6 @@ class HomeController < ApplicationController
 	private
 
 	def set_page_name
-		@page_name = params[:id]
+		@page_name = (params.has_key? :id) ? params[:id] : "@Casetabs"
 	end
 end
